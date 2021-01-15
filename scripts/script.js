0e62abe451bc729c1a783daa7b57a51358758f5f
Cal Richards <calrichards@me.com>
2021-01-15 18:54:57 +0000

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,30 @@
 // ! ************ VARIABLES **************
 // ? js variables
-const width = 11
-const height = 20
+const width = 12
+const height = 21
 const numCells = width * height
 let cellsArray = []
+let rowsCleared = 0
+let score = 0
+let shapeMoving = false
+let blockedCells = []
 // ? object with arrays of shapes starting coords
 const shapeArrays = {
-  squareShape: [4, 5, 14, 15],
-  l1Shape: [4, 5, 6, 14],
-  l2Shape: [4, 5, 6, 16],
-  lineShape: [3, 4, 5, 6],
-  tShape: [4, 5, 6, 15],
-  s1Shape: [4, 5, 15, 16],
-  s2Shape: [4, 5, 13, 14]
+  squareShape: [5, 6, 17, 18],
+  l1Shape: [5, 6, 7, 17],
+  l2Shape: [5, 6, 7, 18],
+  lineShape: [4, 5, 6, 7],
+  tShape: [5, 6, 7, 18],
+  s1Shape: [5, 6, 16, 17],
+  s2Shape: [5, 6, 18, 19]
 }
-
+let activeShapeCoords = []
 // ?  dom variables
 const grid = document.querySelector('.grid')
 const startButton = document.querySelector('.start-button')
 const resetButton = document.querySelector('.reset-button')
 // ! **************************
+
 
 // ! ************ INIT **************
 // ? create grid & cells - apply styling/classes
@@ -34,10 +39,13 @@ for (let i = 0; i < numCells; i++) {
   cell.style.height = `${100 / height}%`
   if (i % width === 0) {
     cell.classList.add('walls')
+    blockedCells.push(cell.id)
   } else if (i % width === width - 1) {
     cell.classList.add('walls')
+    blockedCells.push(cell.id)
   } else if (i + width >= width * height) {
     cell.classList.add('walls')
+    blockedCells.push(cell.id)
   }
 }
 // ! **************************
@@ -45,14 +53,32 @@ for (let i = 0; i < numCells; i++) {
 
 // ! ************ FUNCTIONS **************
 function addShape() {
+  // ? below selects random shape from object and inserts into onto the grid
   const shapeKeys = Object.keys(shapeArrays)
   const randomShapeIndex = Math.floor(Math.random() * shapeKeys.length) 
   const randomShapeKey = shapeKeys[randomShapeIndex] 
   const randomShape = shapeArrays[randomShapeKey]
-  console.log(randomShape);
+  activeShapeCoords = randomShape
+  // console.log(activeShapeCoords)
   randomShape.forEach(element => {
     cellsArray[element].classList.add('shape')        
-  });
+    cellsArray[element].classList.add('active-shape')  
+  })
+  
+  // ? below starts set interval moving shape down the grid
+  const shapeMovementInterval = setInterval(() => {
+    // console.log(activeShapeCoords)
+    for(let i = 0; i < activeShapeCoords.length; i++) {
+      // console.log(cellsArray[activeShapeCoords[i]].id);
+      if(cellsArray[activeShapeCoords[i]] > width && cellsArray[activeShapeCoords[i - width]].classList.contains('active-shape')) {
+        console.log('hi');
+        cellsArray[activeShapeCoords[i]].classList.remove('active-shape')
+      }
+      activeShapeCoords[i] += width
+      cellsArray[activeShapeCoords[i]].classList.add('active-shape')
+      // console.log(activeShapeCoords);
+    }
+  }, 1000);
 }
 addShape()
 
@@ -62,7 +88,16 @@ function startGame() {
 }
 
 function checkCollusion() {
-  
+  activeShapeCoords.forEach(num => {
+    if(blockedCells.includes(num + width)) {
+      shapeMoving = false
+      blockedCells.push(activeShapeCoords)
+      document.getElementById(num).classList.remove('active-shape')
+      document.getElementById(num).classList.add('inactive-shape')
+      addShape()
+    }
+
+  })
 }
 
 function rotateShape() {
