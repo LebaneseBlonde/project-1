0176f86d2f817d28ae34ef21787ffceec51ece41
Cal Richards <calrichards@me.com>
2021-01-19 21:50:56 +0000

    added speed increase difficulty, score and lines cleared display

--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,17 @@ let ableToMoveRight = true
 let hasCollision = false
 let inactiveCells = []
 let gameActive = 0
-let intervalTime = 400
+let currentSpeed = 400
+let intervalTime = currentSpeed
 // ? object with arrays of shapes starting coords
 const shapeArrays = {
-  squareShape: ['0-4', '0-5', '1-4', '1-5'],
-  lineShape: ['0-3', '0-4', '0-5', '0-6'],
-  l1Shape: ['0-3', '0-4', '0-5', '1-5'],
-  l2Shape: ['0-3', '0-4', '0-5', '1-3'],
-  tShape: ['0-4', '0-5', '0-6', '1-5'],
-  s1Shape: ['0-4', '0-5', '1-3', '1-4'],
-  s2Shape: ['0-4', '0-5', '1-5', '1-6']
+  squareShape: ['0_4', '0_5', '1_4', '1_5'],
+  lineShape: ['0_3', '0_4', '0_5', '0_6'],
+  l1Shape: ['0_3', '0_4', '0_5', '1_5'],
+  l2Shape: ['0_3', '0_4', '0_5', '1_3'],
+  tShape: ['0_4', '0_5', '0_6', '1_5'],
+  s1Shape: ['0_4', '0_5', '1_3', '1_4'],
+  s2Shape: ['0_4', '0_5', '1_5', '1_6']
 }
 let activeShapeCoords = []
 // ?  dom variables
@@ -28,6 +29,10 @@ const grid = document.querySelector('.grid')
 const startButton = document.querySelector('.start-button')
 const pauseButton = document.querySelector('.pause-button')
 const resetButton = document.querySelector('.reset-button')
+const scoreDisplay = document.getElementById('score-num')
+const linesDisplay = document.getElementById('lines-num')
+scoreDisplay.innerHTML = score
+linesDisplay.innerHTML = rowsCleared
 // ! **************************
 
 
@@ -40,8 +45,8 @@ for (let row = 0; row < height; row++) {
     cell.classList.add('cells')
     cell.setAttribute('row', row)
     cell.setAttribute('column', column)
-    cell.innerHTML = `${row}-${column}`
-    cell.id = `${row}-${column}`
+    cell.innerHTML = `${row}_${column}`
+    cell.id = `${row}_${column}`
     cell.style.width = `${100 / width}%`
     cell.style.height = `${100 / height}%`
     grid.appendChild(cell)
@@ -74,9 +79,9 @@ function checkCollision() {
   gameOver()
   hasCollision = false
   activeShapeCoords.forEach(coord => {
-    const y = Number(coord.toString().split('-')[0])
-    const x = Number(coord.toString().split('-')[1])
-    if (y === height - 1 || inactiveCells.includes(`${y + 1}-${x}`)) {
+    const y = Number(coord.toString().split('_')[0])
+    const x = Number(coord.toString().split('_')[1])
+    if (y === height - 1 || inactiveCells.includes(`${y + 1}_${x}`)) {
       shapeMoving = false
       hasCollision = true
     }
@@ -97,11 +102,11 @@ function checkShapeMove() {
   ableToMoveLeft = true
   ableToMoveRight = true
   activeShapeCoords.forEach(coord => {
-    const x = Number(coord.toString().split('-')[1])
-    const y = Number(coord.toString().split('-')[0])
-    if (x === 0 || inactiveCells.includes(`${y}-${x - 1}`)) {
+    const x = Number(coord.toString().split('_')[1])
+    const y = Number(coord.toString().split('_')[0])
+    if (x === 0 || inactiveCells.includes(`${y}_${x - 1}`)) {
       ableToMoveLeft = false
-    } else if (x === width - 1 || inactiveCells.includes(`${y}-${x + 1}`)) {
+    } else if (x === width - 1 || inactiveCells.includes(`${y}_${x + 1}`)) {
       ableToMoveRight = false
     }
   })
@@ -111,12 +116,12 @@ function moveShape(move, direction) {
   let newCoords = []
   activeShapeCoords.forEach(coord => {
     document.getElementById(coord).classList.remove('active-shape')
-    const y = Number(coord.toString().split('-')[0])
-    const x = Number(coord.toString().split('-')[1])
+    const y = Number(coord.toString().split('_')[0])
+    const x = Number(coord.toString().split('_')[1])
     if (direction === 'vertical') {
-      newCoords.push(`${y + move}-${x}`)
+      newCoords.push(`${y + move}_${x}`)
     } else if (direction === 'horizontal') {
-      newCoords.push(`${y}-${x + move}`)
+      newCoords.push(`${y}_${x + move}`)
     }
   })
   activeShapeCoords = newCoords
@@ -130,46 +135,61 @@ function rotateShape() {
   let newCoords2 = []
   activeShapeCoords.forEach((coord, index) => {
     document.getElementById(coord).classList.remove('active-shape')
-    let x = Number(coord.toString().split('-')[1])
-    let y = Number(coord.toString().split('-')[0])
+    let x = Number(coord.toString().split('_')[1])
+    let y = Number(coord.toString().split('_')[0])
     const pivotIndex = 1
-    const pivotX = Number(activeShapeCoords[pivotIndex].toString().split('-')[1])
-    const pivotY = Number(activeShapeCoords[pivotIndex].toString().split('-')[0])
+    const pivotX = Number(activeShapeCoords[pivotIndex].toString().split('_')[1])
+    const pivotY = Number(activeShapeCoords[pivotIndex].toString().split('_')[0])
     const relativeX = x - pivotX
     const relativeY = y - pivotY
     const rotatedX = relativeY
     const rotatedY = -relativeX
     x = pivotX + rotatedX
     y = pivotY + rotatedY
-    newCoords.push(`${y}-${x}`)
+    newCoords.push(`${y}_${x}`)
   }) 
-  // ? stops shapes rotating outside the grid on the x axis
+  // ? stops shapes rotating outside the grid on the right side of the x axis
   newCoords.forEach((coord) => {
-    let x = Number(coord.toString().split('-')[1])
-    let y = Number(coord.toString().split('-')[0])
+    let x = Number(coord.toString().split('_')[1])
+    let y = Number(coord.toString().split('_')[0])
     if (x > width -1) {
       newCoords2 = newCoords
       newCoords = []
       newCoords2.forEach(coord2 => {
-        let x = Number(coord2.toString().split('-')[1])
-        let y = Number(coord2.toString().split('-')[0])
+        let x = Number(coord2.toString().split('_')[1])
+        let y = Number(coord2.toString().split('_')[0])
         x--
-        newCoords.push(`${y}-${x}`)
+        newCoords.push(`${y}_${x}`)
       })
     }
   })
-  // ? stops shapes rotating outside the grid on the y axis
+  // ? stops shapes rotating outside the grid on the left side of the x axis
   newCoords.forEach((coord) => {
-    let x = Number(coord.toString().split('-')[1])
-    let y = Number(coord.toString().split('-')[0])
-    if (y < 1) {
+    let x = Number(coord.toString().split('_')[1])
+    let y = Number(coord.toString().split('_')[0])
+    if (x <= 0) {
       newCoords2 = newCoords
       newCoords = []
       newCoords2.forEach(coord2 => {
-        let x = Number(coord2.toString().split('-')[1])
-        let y = Number(coord2.toString().split('-')[0])
+        let x = Number(coord2.toString().split('_')[1])
+        let y = Number(coord2.toString().split('_')[0])
+        x++
+        newCoords.push(`${y}_${x}`)
+      })
+    }
+  })
+  // ? NOT WORKING stops shapes rotating outside the grid on the y axis
+  newCoords.forEach((coord) => {
+    let x = Number(coord.toString().split('_')[1])
+    let y = Number(coord.toString().split('_')[0])
+    if (y <= 0) {
+      newCoords2 = newCoords
+      newCoords = []
+      newCoords2.forEach(coord2 => {
+        let x = Number(coord2.toString().split('_')[1])
+        let y = Number(coord2.toString().split('_')[0])
         y++
-        newCoords.push(`${y}-${x}`)
+        newCoords.push(`${y}_${x}`)
       })
     }
   })
@@ -182,27 +202,30 @@ function rotateShape() {
 
 function clearRow() {
   let rowCounts = {}
-  const yCoords = inactiveCells.map(coord => coord.split('-')[0])
+  const yCoords = inactiveCells.map(coord => coord.split('_')[0])
   yCoords.forEach(num => rowCounts[num] = (rowCounts[num] || 0) + 1)
   for (const key in rowCounts) {
     if (rowCounts[key] === width) {
       rowsCleared++
+      score += 100
+      scoreDisplay.innerHTML = score
+      linesDisplay.innerHTML = rowsCleared
       for (let i = 0; i < width; i++) {
-        document.getElementById(`${key}-${i}`).classList.remove('inactive-shape')
+        document.getElementById(`${key}_${i}`).classList.remove('inactive-shape')
       }
       inactiveCells = inactiveCells.filter(coord => {
-        const y = Number(coord.split('-')[0])
+        const y = Number(coord.split('_')[0])
         return y !== Number(key)
       })
       let oldCells = []
       let newCells = []
       inactiveCells = inactiveCells.map(coord => {
-        const y = Number(coord.split('-')[0])
-        const x = Number(coord.split('-')[1])
+        const y = Number(coord.split('_')[0])
+        const x = Number(coord.split('_')[1])
         if (y < key) {
           oldCells.push(coord)
-          newCells.push(`${y + 1}-${x}`)
-          return `${y + 1}-${x}`
+          newCells.push(`${y + 1}_${x}`)
+          return `${y + 1}_${x}`
         } else return coord
       })
       oldCells.forEach(cell => document.getElementById(cell).classList.remove('inactive-shape'))
@@ -228,7 +251,7 @@ function resetGame() {
 
 function gameOver() {
   inactiveCells.forEach(coord => {
-    const y = Number(coord.toString().split('-')[0])
+    const y = Number(coord.toString().split('_')[0])
     if (y === 0) {
       shapeMoving = false
       ableToMoveLeft = false
@@ -244,6 +267,17 @@ function gameOver() {
 function shapeMovementTimeout() {
   if (shapeMoving) {
     moveShape(1, 'vertical')
+  }
+  if (intervalTime === 50) {
+    score++
+    scoreDisplay.innerHTML = score
+  }
+  if (rowsCleared === 10) { 
+    intervalTime = 325
+  } else if (rowsCleared === 20) {
+    intervalTime = 225
+  } else if (rowsCleared === 30) {
+    intervalTime = 175
   }
   checkShapeMove()
   checkCollision()
@@ -286,7 +320,7 @@ document.addEventListener('keydown', (event) => {
   if (key === 'a' && !hasCollision && ableToMoveLeft && gameActive && shapeMoving) {
      moveShape(-1, 'horizontal')
   } else if (key === 's' && !hasCollision && gameActive && shapeMoving) {
-    intervalTime = 55
+    intervalTime = 50
     // console.log('im fast as fuck boiiii!');
   } else if (key === 'd' && !hasCollision && ableToMoveRight && gameActive && shapeMoving) {
     moveShape(1, 'horizontal')
@@ -296,7 +330,7 @@ document.addEventListener('keydown', (event) => {
 document.addEventListener('keyup', (event) => {
   const key = event.key
   if (key === 's' && !hasCollision && gameActive ) {
-    intervalTime = 400
+    intervalTime = currentSpeed
   } else if (key === 'w' && !hasCollision && gameActive && shapeMoving) {
     rotateShape()
   }
