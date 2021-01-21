e953021141330092880e0bf3d5e7c32ddc1f2064
Cal Richards <calrichards@me.com>
2021-01-21 11:52:06 +0000

    added audio element, audio files for add shape, and js logic to play

--- scripts/script.js
+++ scripts/script.js
@@ -16,17 +16,19 @@ let currentSpeed = 400
 let intervalTime = currentSpeed
 let rotation = 0
 let playAudio = true
-// ? object with arrays of shapes starting coords
-const iTet = { coords: ['0_3', '0_4', '0_5', '0_6'], background: ['images/i1.png', 'images/i2.png', 'images/i3.png', 'images/i4.png'], pivot: 1, canRotate: true }
-const jTet = { coords: ['0_4', '1_4', '1_5', '1_6'], background: ['images/j1.png', 'images/j2.png', 'images/j3.png', 'images/j4.png'], pivot: 2, canRotate: true }
-const lTet = { coords: ['0_5', '1_3', '1_4', '1_5'], background: ['images/l1.png', 'images/l2.png', 'images/l3.png', 'images/l4.png'], pivot: 2, canRotate: true }
-const oTet = { coords: ['0_4', '0_5', '1_4', '1_5'], background: ['images/o1.png', 'images/o2.png', 'images/o3.png', 'images/o4.png'], pivot: 1, canRotate: false }
-const sTet = { coords: ['0_4', '0_5', '1_3', '1_4'], background: ['images/s1.png', 'images/s2.png', 'images/s3.png', 'images/s4.png'], pivot: 3, canRotate: true }
-const tTet = { coords: ['0_4', '1_3', '1_4', '1_5'], background: ['images/t1.png', 'images/t2.png', 'images/t3.png', 'images/t4.png'], pivot: 2, canRotate: true }
-const zTet = { coords: ['0_4', '0_5', '1_5', '1_6'], background: ['images/z1.png', 'images/z2.png', 'images/z3.png', 'images/z4.png'], pivot: 2, canRotate: true }
+
+// ? shape objects
+const iTet = { coords: ['0_3', '0_4', '0_5', '0_6'], background: ['images/i1.png', 'images/i2.png', 'images/i3.png', 'images/i4.png'], audio: '1.mp3', pivot: 1, canRotate: true }
+const jTet = { coords: ['0_4', '1_4', '1_5', '1_6'], background: ['images/j1.png', 'images/j2.png', 'images/j3.png', 'images/j4.png'], audio: '2.mp3', pivot: 2, canRotate: true }
+const lTet = { coords: ['0_5', '1_3', '1_4', '1_5'], background: ['images/l1.png', 'images/l2.png', 'images/l3.png', 'images/l4.png'], audio: '3.mp3', pivot: 2, canRotate: true }
+const oTet = { coords: ['0_4', '0_5', '1_4', '1_5'], background: ['images/o1.png', 'images/o2.png', 'images/o3.png', 'images/o4.png'], audio: '4.mp3', pivot: 1, canRotate: false }
+const sTet = { coords: ['0_4', '0_5', '1_3', '1_4'], background: ['images/s1.png', 'images/s2.png', 'images/s3.png', 'images/s4.png'], audio: '5.mp3', pivot: 3, canRotate: true }
+const tTet = { coords: ['0_4', '1_3', '1_4', '1_5'], background: ['images/t1.png', 'images/t2.png', 'images/t3.png', 'images/t4.png'], audio: '6.mp3', pivot: 2, canRotate: true }
+const zTet = { coords: ['0_4', '0_5', '1_5', '1_6'], background: ['images/z1.png', 'images/z2.png', 'images/z3.png', 'images/z4.png'], audio: '7.mp3', pivot: 2, canRotate: true }
 let randomShape = {}
 let activeShapeCoords = []
-// ?  dom variables
+
+// ?  dom elements
 const grid = document.querySelector('.grid')
 const startScreen = document.querySelector('.start-screen')
 const gameoverScreen = document.querySelector('.gameover-screen')
@@ -48,6 +50,7 @@ const linesDisplay = document.getElementById('lines-num')
 const levelDisplay = document.getElementById('level-num')
 const gridLeft = document.querySelector('.grid-left-buttons')
 const gridRight = document.querySelector('.grid-right-scores')
+const audioPlayer = document.querySelector('audio')
 // ! **************************
 
 
@@ -95,6 +98,8 @@ function addShape() {
     const shape = document.getElementById(coord)
     shape.classList.add('active-shape')  
   })
+  audioPlayer.src =  `/audio/${randomShape.audio}`
+  audioPlayer.play()
 }
 
 function checkCollision() {
